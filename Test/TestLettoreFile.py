cb5ee01ebad696ef947f7b487a1d33da7fef96f9
Chris0451 <dcchris56@gmail.com>
2025-02-18 16:22:07 +0100

    Aggiornati TestLettoreFile e Corriere

--- Test/TestLettoreFile.py
+++ Test/TestLettoreFile.py
@@ -69,8 +69,62 @@ class TestLettoreFile(unittest.TestCase):
             [['Cristian', 'Di Cintio', 'CD201103K', '3893821832', 'cristian.dc@example.com', 'CL0002', ['Via Bartolo', 1, 'Ancona', 'AN', 66019]], 
             'F001', 'DC Group', '12:30', '18:30', ['Lunedi', 'Martedi', 'Mercoledi', 'Giovedi', 'Venerdi']],
             ['C220', '07/02/2025', '14:00', 'in deposito']]
+            ***********
             """)
-    def test_leggi_colli(self, mock_file):
+    def test_leggi_colli_consegna(self, mock_file):
+        lettore_file = LettoreFile()
+        colli = lettore_file.leggi_lista_colli_consegne()
+
+        # Verifica che il file sia stato aperto correttamente
+        mock_file.assert_called_with("Dati/listaColliRitiri.txt", "r")
+        
+        # Verifica che i dati siano stati correttamente letti e trasformati in oggetti Collo
+        self.assertEqual(len(colli), 1)  # Dovremmo avere un collo
+        collo = colli[0]
+        
+        # Verifica attributi del collo
+        self.assertIsInstance(collo, Collo)
+        self.assertEqual(collo.codice_collo, '1')
+        self.assertEqual(collo.natura_collo, 'Elettronica')
+        self.assertEqual(collo.peso, 20.0)
+        self.assertEqual(collo.volume, 30.0)
+        
+        # Verifica che i dati del mittente siano corretti
+        self.assertIsInstance(collo.mittente, Cliente)
+        self.assertEqual(collo.mittente.nome, 'Alberto')
+        self.assertEqual(collo.mittente.posizione.via, 'Via Michele Fazioli')
+        
+        # Verifica che i dati del destinatario siano corretti
+        self.assertIsInstance(collo.destinatario, Cliente)
+        self.assertEqual(collo.destinatario.nome, 'Cristian')
+        self.assertEqual(collo.destinatario.posizione.via, 'Via Bartolo')
+        
+        # Verifica che i dati dell'azienda mittente e destinatario siano corretti
+        self.assertIsInstance(collo.mittente_azienda, Azienda)
+        self.assertEqual(collo.mittente_azienda.nome_azienda, 'Festa Group')
+        self.assertEqual(collo.mittente_azienda.orario_apertura, '8:30')
+        
+        self.assertIsInstance(collo.destinatario_azienda, Azienda)
+        self.assertEqual(collo.destinatario_azienda.nome_azienda, 'DC Group')
+        self.assertEqual(collo.destinatario_azienda.orario_chiusura, '18:30')
+        
+        # Verifica che il ritiro sia stato correttamente creato
+        self.assertIsInstance(collo.ritiro, Ritiro)
+        self.assertEqual(collo.ritiro.codice_ritiro, 'C220')
+        self.assertEqual(collo.ritiro.data_ritiro, '07/02/2025')
+        self.assertEqual(collo.ritiro.ora_ritiro, '14:00')
+        self.assertEqual(collo.ritiro.stato_ritiro, 'in deposito')
+
+    @patch("builtins.open", new_callable=mock_open, read_data=""" 
+            ["1", "Elettronica", 20.0, 30.0, 
+            [["Alberto", "Festa", "AF200803H", "3773821832", "alberto.festa@example.com", "CL0001", ["Via Michele Fazioli", 1, "Ancona", "AN", 66019]], 
+            "F000", "Festa Group", "8:30", "18:30", ["Lunedi", "Martedi", "Mercoledi", "Giovedi", "Venerdi"]],
+            [["Cristian", "Di Cintio", "CD201103K", "3893821832", "cristian.dc@example.com", "CL0002", ["Via Bartolo", 1, "Ancona", "AN", 66019]], 
+            "F001", "DC Group", "8:30", "18:30", ["Lunedi", "Martedi", "Mercoledi", "Giovedi", "Venerdi"]],
+            ["R220", "05/02/2025", "14:00", "in deposito"]]
+            ***********
+            """)
+    def test_leggi_colli_ritiro(self, mock_file):
         lettore_file = LettoreFile()
         colli = lettore_file.leggi_lista_colli()
 
@@ -109,7 +163,7 @@ class TestLettoreFile(unittest.TestCase):
         
         # Verifica che il ritiro sia stato correttamente creato
         self.assertIsInstance(collo.ritiro, Ritiro)
-        self.assertEqual(collo.ritiro.codice_ritiro, 'C220')
+        self.assertEqual(collo.ritiro.codice_ritiro, 'R220')
         self.assertEqual(collo.ritiro.data_ritiro, '07/02/2025')
         self.assertEqual(collo.ritiro.ora_ritiro, '14:00')
         self.assertEqual(collo.ritiro.stato_ritiro, 'in deposito')
--- Utenti/Corriere.py
+++ Utenti/Corriere.py
@@ -1,8 +1,8 @@
-from Utente import *
+from Utenti.Utente import Utente
 class Corriere(Utente):
     def __init__(self, nome, cognome, codiceFiscale, data_nascita, telefono, email, identificativo):
-        super().__init__(nome, cognome, codiceFiscale, data_nascita, telefono, email, password, identificativo)
+        super().__init__(nome, cognome, codiceFiscale, data_nascita, telefono, email, identificativo)
         self.identificativo = identificativo
     def infoCorriere(self):
-        return infoUtente(self) + ", Identificativo corriere: {self.identificativo}"
+        return self.getInfoUtente() + "\nIdentificativo corriere: {self.identificativo}"
     
